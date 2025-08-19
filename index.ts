0f4dceb16878e24df0ead50fe3c5fc881d132168
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-08-19 21:58:51 +0100

    refactor: switch to commonjs and add tsconfig
    - Modify CORS configuration and add development server

--- index.ts
+++ index.ts
@@ -7,14 +7,19 @@ import routes from "./routes/index";
 dotenv.config();
 const app = express();
 
-
 const clientUrl = process.env.CLIENT_URL;
+const PORT = process.env.PORT || 13200;
+
 const corsOptions = {
   credentials: true,
-  origin: clientUrl ? clientUrl.split(',') : [],
+  origin: clientUrl ? clientUrl.split(",") : ["http://localhost:3000"],
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", routes);
+
+if (process.env.NODE_ENV === "development") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 module.exports = app;

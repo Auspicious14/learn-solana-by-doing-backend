91497e085002fc3f2b11bc11cd92148afe6027b7
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-21 11:36:51 +0100

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { solanaProxy } from "../config/proxy";
 import { getBalance } from "../balance";
+import { getNFTsByOwner } from "../nft/index"
 
 const router = Router();
 
 router.post("/proxy", solanaProxy);
 router.post("/get-balance", getBalance);
+router.post("/nfts/get-by-owner", getNFTsByOwner as any)
 
 export default router;
